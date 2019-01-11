91ec0a95c6e1744523f18d4bade2c49519b55219
LincolnxBorges <44812778+LincolnxBorges@users.noreply.github.com>
2019-01-11 16:12:02 -0200

    Update Teste.java

--- Teste_Linux/src/Teste.java
+++ Teste_Linux/src/Teste.java
@@ -3,6 +3,7 @@ public class Teste {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
+		System.out.println("Hello Git");
 		//Alteração feito pelo Eclipse
 		//Alteração feito pelo Git
 	}
