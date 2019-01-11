e3044507d383d46c74abd76e5ca38a21d8094651
Lincoln Borges <clauber_borges@carrefour.com>
2019-01-11 16:05:04 -0200

    Nota

--- Teste_Linux/src/Teste.java
+++ Teste_Linux/src/Teste.java
@@ -3,8 +3,9 @@ public class Teste {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		//Alteração feito pelo Eclipse
-		//Alteração feito pelo Git
+		double p1 = 9;
+		double p2 = 6;
+		double notaFinal = (p1 + p2) /2;
+		System.out.println("Sua nota final é: " + notaFinal);
 	}
-
 }
