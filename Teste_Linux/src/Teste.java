d3a88dd07fe08295d5124a2139e76a8ac20e3a13
Lincoln Borges <clauber_borges@carrefour.com>
2019-01-11 16:08:54 -0200

    Nota 2

--- Teste_Linux/src/Teste.java
+++ Teste_Linux/src/Teste.java
@@ -4,7 +4,7 @@ public class Teste {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		double p1 = 9;
-		double p2 = 6;
+		double p2 = 7;
 		double notaFinal = (p1 + p2) /2;
 		System.out.println("Sua nota final é: " + notaFinal);
 	}
