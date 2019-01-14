f2d972623be5831e97540bb47d9cf9e8d20cc6d2
LincolnxBorges <44812778+LincolnxBorges@users.noreply.github.com>
2019-01-14 14:54:31 -0200

    Update Teste.java

--- Teste_Linux/src/Teste.java
+++ Teste_Linux/src/Teste.java
@@ -3,8 +3,8 @@ public class Teste {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
-		double p1 = 9;
-		double p2 = 7;
+		double p1 = 5;
+		double p2 = 5;
 		double notaFinal = (p1 + p2) /2;
 		
 		if (notaFinal > 6){
